91d45c9380fd58f47565ccd367bd295b8563fca4
Jason Stangroome <jason@section.io>
2017-12-27 11:08:27 +1100

    Terraform HCL style

--- example.tf
+++ example.tf
@@ -1,11 +1,11 @@
 provider "rackcorp" {
-	api_uuid = "the-uuid-from-tf"
-	api_secret = "the-secret-from-tf"
-	customer_id = "001122"
+  api_uuid = "the-uuid-from-tf"
+  api_secret = "the-secret-from-tf"
+  customer_id = "001122"
 }
 
 resource "rackcorp_server" "example" {
-	country = "the-country-from-tf"
-	server_class = "the-server_class-from-tf"
-	operating_system = "the-operating-system-from-tf"
+  country = "the-country-from-tf"
+  server_class = "the-server_class-from-tf"
+  operating_system = "the-operating-system-from-tf"
 }
