b844b22808368964f34b4a7537416901403c3694
Mani Batra <manibatra2002@gmail.com>
2019-10-21 17:14:05 +1100

    Fixed env var

--- test-apply.sh
+++ test-apply.sh
@@ -8,9 +8,9 @@ terraform () {
   docker run \
     --rm \
     -v "${SCRIPT_ROOT}/:/app/:rw" \
-    -e RACKCORP_API_UUID \
-    -e RACKCORP_API_SECRET \
-    -e RACKCORP_CUSTOMER_ID \
+    -e RACKCORP_API_UUID="$RACKCORP_API_UUID" \
+    -e RACKCORP_API_SECRET="$RACKCORP_API_SECRET" \
+    -e RACKCORP_CUSTOMER_ID="$RACKCORP_CUSTOMER_ID" \
     -w /app/ \
     "${terraform_image}" \
       "$@"
