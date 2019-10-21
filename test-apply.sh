6bf0b1c82962bfd799d2d22d96fff50cfbe3b74e
Mani Batra <manibatra2002@gmail.com>
2019-10-21 16:49:43 +1100

    Added helper script for testing

---
+++ test-apply.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+readonly SCRIPT_ROOT=$(pwd)
+terraform_image=gcr.io/section-io/terraform-provider-rackcorp:latest
+# ensure latest
+docker pull "${terraform_image}"
+
+terraform () {
+  docker run \
+    --rm \
+    -v "${SCRIPT_ROOT}/:/app/:rw" \
+    -e RACKCORP_API_UUID \
+    -e RACKCORP_API_SECRET \
+    -e RACKCORP_CUSTOMER_ID \
+    -w /app/ \
+    "${terraform_image}" \
+      "$@"
+}
+
+terraform fmt -diff -write=false
+
+terraform init -input=false
+
+terraform plan \
+  -out apply.tfplan
+
+if [ 'true' = "${SKIP_APPLY}" ]
+then
+  printf 'Skipping "terraform apply".\n'
+  exit
+fi
+
+terraform apply apply.tfplan
+
