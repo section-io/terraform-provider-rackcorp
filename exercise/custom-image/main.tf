773b5912769b2d904af4a4215647b7422c08b639
Jason Stangroome <jason@section.io>
2020-05-27 08:15:54 +1000

    Remove exercise image bucket and path from committed files

--- exercise/custom-image/main.tf
+++ exercise/custom-image/main.tf
@@ -2,6 +2,8 @@ provider "rackcorp" {}
 
 variable "image_access_key" {}
 variable "image_access_secret" {}
+variable "image_bucket" {}
+variable "image_path" {}
 
 resource "rackcorp_server" "exercise" {
   name         = "custom-image"
@@ -12,10 +14,10 @@ resource "rackcorp_server" "exercise" {
   memory_gb    = 4
   user_data    = "${file("${path.module}/user_data.yaml")}"
 
-  deploy_media_image_bucket        = "rackcorp-bucket-name"
   deploy_media_image_access_key    = "${var.image_access_key}"
   deploy_media_image_access_secret = "${var.image_access_secret}"
-  deploy_media_image_path          = "image-b0093e2.qcow2"
+  deploy_media_image_bucket        = "${var.image_bucket}"
+  deploy_media_image_path          = "${var.image_path}"
 
   host_group_id = 4 # Not all hosts support deploy_media_image_* yet
 
